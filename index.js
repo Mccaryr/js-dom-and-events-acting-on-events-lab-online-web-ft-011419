f38aeef61e091503030e07bbdc4569e9e69ea64c
Robert McCary <robert.mccary@yahoo.com>
2019-04-26 00:34:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ function retrieveEmployeeInformation(){
 }
 
 function addNewElementAsLi(){
-  const unorderedList = document.querySelector('ul')
-  const list = document.createElement('li')
+  const unorderedList = document.querySelector("ul")
+  const list = document.createElement("li")
   list.innerHTML = retrieveEmployeeInformation()
   list.appendChild(unorderedList);
 }
